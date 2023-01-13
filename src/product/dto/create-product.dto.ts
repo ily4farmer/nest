11451fb3c47dba0ce695af1e1e83ac73545fcce6
Pavel S <pavels@Alexeys-Mac-mini.local>
2023-01-13 23:39:11 +0300

    add swager for product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,15 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductDto {
+  @ApiProperty({ example: 'лого', description: 'имя категории' })
+  readonly logo: string;
+
+  @ApiProperty({ example: 'Зеленые шорты', description: 'имя продукта' })
+  readonly name: string;
+
+  @ApiProperty({ example: 'Цена товара', description: 'имя продукта' })
+  readonly price: number;
+
+  @ApiProperty({ example: 'Красивые шорты', description: 'описание продукта' })
+  readonly description: string;
+}
--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   BelongsTo,
   Column,
@@ -10,18 +11,29 @@ import { Category } from 'src/category/category.model';
 
 @Table({ tableName: 'product' })
 export class Product extends Model {
+  @ApiProperty({ example: 1, description: 'Уникальный идентификатор' })
   @Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true })
   id: number;
 
+  @ApiProperty({
+    example: 'base64:ffdgdf',
+    description: 'картинка в формате base64',
+  })
   @Column({ type: DataType.STRING })
   logo: string;
 
+  @ApiProperty({ example: 'Fable green', description: 'имя категории' })
   @Column({ type: DataType.STRING, allowNull: false, unique: true })
   name: string;
 
+  @ApiProperty({ example: 456, description: 'цена товара' })
   @Column({ type: DataType.INTEGER, allowNull: false })
   price: number;
 
+  @ApiProperty({
+    example: 'лучшие шорты в мире',
+    description: 'описание товара',
+  })
   @Column({ type: DataType.STRING, allowNull: false, defaultValue: '' })
   description: string;
 
